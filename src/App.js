78bed2977e3b36556e0395ecc160aed2932edca6
Sarobidy <nomenjanaharytonyo@gmail.com>
2018-10-04 19:56:07 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import NavbarHeader from './components/Navbar';
 
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <NavbarHeader />
+      
+        <Footer />  
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import App from './App';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render( <App /> , document.getElementById('root'));
-
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
